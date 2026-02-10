180549d0ce967eb0da50eb89a6e4c47c010b0b56
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2026-02-10 21:18:55 +0530

    Implement DateCounter component with useReducer for state management

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  console.log(state, action);
+  if (action.type === "dec") return state - 1;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "setCount") return action.payload;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -9,17 +16,15 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
@@ -27,17 +32,17 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
   return (
-    <div className="counter">
+    <div className='counter'>
       <div>
         <input
-          type="range"
-          min="0"
-          max="10"
+          type='range'
+          min='0'
+          max='10'
           value={step}
           onChange={defineStep}
         />
